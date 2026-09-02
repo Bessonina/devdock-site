20ace15a36423dfb7ac352502ad659b7c385be93
Bessonina <bessonov.yuri@bk.ru>
2026-09-02 19:06:18 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,24 @@
+// app/layout.tsx
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Стройка Девдок',
+  description: 'Честный опыт самостройщика',
+  verification: {
+    other: {
+      'takprodam-verification': '3ec5e2ad-a511-46d2-bf46-cb5448a6b96a',
+    },
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body>{children}</body>
     </html>
-  );
+  )
 }
